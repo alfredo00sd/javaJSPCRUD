7b267296ef8a0fcea2cb35c6124bac7f143ad3ed
alfredo00sd <alfredoz28mm@gmail.com>
2020-07-04 10:41:17 -0400

    [CLEANING] project from file shit!

--- src/main/java/Controller/CustomerController.java
+++ src/main/java/Controller/CustomerController.java
@@ -2,27 +2,16 @@ package Controller;
 
 import DAO.CustomerDAO;
 import Model.CustomerBean;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class CustomerController extends HttpServlet {
@@ -35,90 +24,9 @@ public class CustomerController extends HttpServlet {
     CustomerBean customerBean;
     CustomerDAO dao = new CustomerDAO();
 
-    private final String PHOTO_TYPE = ".jpg";
-    private int maxFileSize = 50 * 1024;
-    private int maxMemSize = 4 * 1024;
-    private boolean isMultipart;
-    private String filePath;
-    private File file ;
-
-    public void init( ){
-        // Get the file location where it would be stored.
-        filePath = getServletContext().getInitParameter("file-upload");
-    }
-
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
 
-        // Check that we have a file upload request
-        //isMultipart = ServletFileUpload.isMultipartContent(request);
-        response.setContentType("text/html");
-        java.io.PrintWriter out = response.getWriter( );
-
-//        if( !isMultipart ) {
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet upload</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<p>No data uploaded</p>");
-//            out.println("</body>");
-//            out.println("</html>");
-//            return;
-//        }
-
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-
-        // maximum size that will be stored in memory
-        factory.setSizeThreshold(maxMemSize);
-
-        // Location to save data that is larger than maxMemSize.
-        factory.setRepository(new File("c:\\temp"));
-
-        // Create a new file upload handler
-        ServletFileUpload upload = new ServletFileUpload(factory);
-
-        // maximum file size to be uploaded.
-        upload.setSizeMax( maxFileSize );
-
-        try {
-            // Parse the request to get file items.
-            List fileItems = upload.parseRequest(request);
-
-            // Process the uploaded file items
-            Iterator i = fileItems.iterator();
-
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet upload</title>");
-            out.println("</head>");
-            out.println("<body>");
-
-            while ( i.hasNext () ) {
-                FileItem fi = (FileItem)i.next();
-                if ( !fi.isFormField () ) {
-                    // Get the uploaded file parameters
-                    String fieldName = fi.getFieldName();
-                    String fileName = fi.getName();
-                    String contentType = fi.getContentType();
-                    boolean isInMemory = fi.isInMemory();
-                    long sizeInBytes = fi.getSize();
-
-                    // Write the file
-                    if( fileName.lastIndexOf("\\") >= 0 ) {
-                        file = new File( filePath + fileName.substring( fileName.lastIndexOf("\\"))) ;
-                    } else {
-                        file = new File( filePath + fileName.substring(fileName.lastIndexOf("\\")+1)) ;
-                    }
-                    fi.write( file ) ;
-                    out.println("Uploaded Filename: " + fileName + "<br>");
-                }
-            }
-            out.println("</body>");
-            out.println("</html>");
-        } catch(Exception ex) {
-            System.out.println(ex);
-        }
     }
 
     @Override
@@ -200,23 +108,6 @@ public class CustomerController extends HttpServlet {
             customer.setFavoriteColor(request.getParameter("inputColor"));
         }
         return customer;
-    }
-
-    private String getSavedServerPhotoUrl(HttpServletRequest request, File uploadedFile) {
-
-        Path folder = Paths.get(request.getContextPath()+"/assets/photos/");
-        String filename = request.getParameter("inputName") +"00"+dao.getLastId();
-        Path file = null;
-
-        try (InputStream input = new FileInputStream(uploadedFile)) {
-
-            file = Files.createTempFile(folder, filename, PHOTO_TYPE);
-            Files.copy(input, file, StandardCopyOption.REPLACE_EXISTING);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return folder + filename + PHOTO_TYPE;
     }
 
     private String getCheckedHobbies(String[] checkHobbiesList) {
